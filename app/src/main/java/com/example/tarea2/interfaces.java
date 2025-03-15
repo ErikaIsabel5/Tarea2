e9bf3a3c84b995783070fa329201c3028bfad5dc
Erika <erikaisabelcastillosantiago@gmail.com>
2025-03-14 22:49:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tarea2/interfaces.java
@@ -0,0 +1,15 @@
+package com.example.tarea2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class interfaces extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inventario);
+        //setContentView(R.layout.pedidos);
+    }
+}
+
+//Vamos a cambiar de interfaz colocando una en modo comentario, ya que de momento no interactuan entre ellas.
